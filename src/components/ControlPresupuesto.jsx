efa1de63af1b0645ee4daccf950ec61ecf85a63f
kikonavarro <78880702+kikonavarro@users.noreply.github.com>
2022-08-30 11:43:54 +0200

    primer commit control presupuesto empezado

---
+++ src/components/ControlPresupuesto.jsx
@@ -0,0 +1,33 @@
+function ControlPresupuesto({presupuesto}) {
+    
+    const formatearCantidad = (cantidad) => {
+        return cantidad.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
+         })
+    }
+    return (
+        <div className="contenedor-presupuesto contenedor sombra dos-columnas">
+            <div>
+                <p>Grafica aqui</p>
+            </div>
+
+            <div className="contenido-presupuesto">
+                <p>
+                    <span>Presupuesto: </span> {formatearCantidad(presupuesto) }
+                </p>
+
+                <p className={`${disponible < 0 ? 'negativo' : '' }`}>
+                    <span>Disponible: </span>{formatearCantidad(0)}
+                </p>
+
+                <p>
+                    <span>Gastado: </span>{formatearCantidad(0)}
+                </p>
+
+            </div>
+        </div>
+      );
+}
+
+export default ControlPresupuesto
