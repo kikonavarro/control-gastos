13c9148b389c50216461bacd8b61be090aefa31b
kikonavarro <78880702+kikonavarro@users.noreply.github.com>
2022-09-02 18:23:29 +0200

    funcionalidad editar gasto

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,6 +1,6 @@
 import Gasto from "./Gasto";
 
-function ListadoGastos({gastos}) {
+function ListadoGastos({gastos, setGastoEditar, eliminarGasto}) {
     return ( 
        <div className="listado-gastos contenedor">
             <h2>{gastos.length ? 'Gastos' : ' No hay gastos todavía'}</h2>
@@ -8,8 +8,9 @@ function ListadoGastos({gastos}) {
             {gastos.map(gasto => (
                 <Gasto
                     key={gasto.id}
-
                     gasto={gasto}
+                    setGastoEditar={setGastoEditar}
+                    eliminarGasto={eliminarGasto}
                 />
             ))}
        </div> 
